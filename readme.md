3cc6bd08a6a4cdb386f268cb438cc131bd8b58fc
Iain Moncrief <Iainmoncrief@gmail.com>
2021-12-23 19:21:53 -1000

    done

---
+++ readme.md
@@ -0,0 +1,20 @@
+# Haskell PDA
+
+
+```haskell
+gram = constructGrammar [('S',"aSa")
+                        ,('S',"bSb")
+                        ,('S',"c")]
+mach = cfgToPda gram
+
+*Main CFG PDA Utilities> mapM_ (putStrLn . show) $ runPDA mach "abacaba"
+(False,"",fromList [(Q 0,"#"),(Q 1,"S#"),(Q 1,"aSa#"),(Q 1,"bSb#"),(Q 1,"c#")])
+(False,"a",fromList [(Q 1,"Sa#"),(Q 1,"aSaa#"),(Q 1,"bSba#"),(Q 1,"ca#")])
+(False,"ab",fromList [(Q 1,"Sba#"),(Q 1,"aSaba#"),(Q 1,"bSbba#"),(Q 1,"cba#")])
+(False,"aba",fromList [(Q 1,"Saba#"),(Q 1,"aSaaba#"),(Q 1,"bSbaba#"),(Q 1,"caba#")])
+(False,"abac",fromList [(Q 1,"aba#")])
+(False,"abaca",fromList [(Q 1,"ba#")])
+(False,"abacab",fromList [(Q 1,"a#")])
+(True,"abacaba",fromList [(Q 1,"#"),(Q 2,"")])
+```
+
--- src/PDA.hs
+++ src/PDA.hs
@@ -40,31 +40,9 @@ type Q = State
 type Γ = Char
 type Γ' = [Γ]
 
--- delta \subset Q x (Σ ⋃ {ε}) x Γ x Q x Γ*
-type TransitionFunction' = Q -> Σ -> Γ -> (Q,Γ')
-
 type StackEdit a = Stack a -> Stack a
 
 data Machine = M { q0 :: State, qfs :: Set State, delta :: TransitionFunction }
-
--- trans :: Transition -> Maybe Char -> PDAState -> Maybe [PDAState]
--- trans (q,(Just c,pop,push),q') Nothing state = Nothing
--- trans tr@(q,(Just c,pop,push),q') (Just c') state = transToTransFunction tr (Just c') state
--- trans tr@(q,(Nothing,pop,push),q') (Just c') state@(q1,stk) = Just [trans q 'q pop push stk, transToTransFunction tr (Just c') state]
--- trans (q,(Nothing,pop,push),q') Nothing (_,stk) = trans' q q' pop push stk
-
--- loopEpsilon pop push q0 q' (q,stk) | q0 == q = loopEpsilon' pop push q' (q,stk)
---                                    | otherwise          = (q,stk) : []
---   where loopEpsilon' (Just c) push _ (q,(c':cs)) = (q,[]) : []
---         loopEpsilon' (Just c) push q' (q,(c':cs)) = (q,(c':cs)) : if c' == cs then loopEpsilon' (Just c) push q0 q' (q',cs)
---         loopEpsilon Nothing push q' (q,stk) = (q,stk) : loopEpsilon
-
--- trans' q q' Nothing push stk = Just (q',push ++ stk) >>= loopEpsilon Nothing push q q' (q',push ++ stk)          
--- trans' q q' (Just c) push stk = case stk
---                                 of {(c':cs) -> if c' == c
---                                               then trans' q q' Nothing push cs >>= loopEpsilon (Just c) push q q'
---                                               else Nothing;
---                                    [] -> Nothing}
 
 transGraphFromList :: [Transition] -> TransitionGraph
 transGraphFromList trs = Map.fromListWith Set.union [(q,Set.singleton (q,tr,q')) | (q,tr,q') <- trs]
@@ -91,82 +69,60 @@ possibleTransitions g q = Set.toList $ Map.findWithDefault Set.empty q g
 possibleTransFunctions g q = map transToTransFunction $ possibleTransitions g q
 
 transGraphToDeltaTrans :: TransitionGraph -> TransitionFunction
-transGraphToDeltaTrans g c (q,stk) = cleanList $ foldl (++) [] $ map appRule $ Set.toList $ Map.findWithDefault Set.empty q g
-    where appRule tr =  [transToTransFunction tr c (q,stk)] -- : (tailWithDefault 3  $ take 2 $ drop $ map Just $ epsilonLoopTrans tr c (q,stk))
-          epsilonLoopTrans tr@(q,trr,q') (Just c) state@(q1,stk) = if q' == q1
-                                                              then epsilonLoopTrans' tr (Just c) state
-                                                              else []
-          epsilonLoopTrans _ _ _ = []
-          epsilonLoopTrans' tr@(q,(Nothing,Nothing,push),q') char state@(q1,stk) = let state' = (q',push ++ stk) in state : epsilonLoopTrans tr char state'
-        -- epsilonLoopTrans' tr@(q,(Nothing,Just c,push),q') char state@(q1,stk)   = state : case stk of { (c':cs) -> if c == c' then (let state' = (q',push ++ cs) in epsilonLoopTrans tr char state') else [];
-        --                                                                                                 []      -> [] } 
-          epsilonLoopTrans' _ _ _ = []
-          tailWithDefault 0 (_:xs) = xs 
-          tailWithDefault n (_:xs) = tailWithDefault (n-1) xs
+transGraphToDeltaTrans g c (q,stk) = cleanList $ map appRule $ Set.toList $ Map.findWithDefault Set.empty q g
+    where appRule tr =  transToTransFunction tr c (q,stk) 
 
--- take :: Int -> [a] -> [a]
--- take n xs     = if n >= 1 
---                 then take n xs : take (n-1) xs
---                 else []
--- take n (x:xs) = x : take (n-1) xs 
-
-
-deltaOf = transGraphToDeltaTrans
--- transGraphToDeltaTrans g c (q,stk) = cleanList $ posTrans
---   where posTrans' = possibleTransFunctions g q
---         posTrans  = map (\tr -> tr c (q,stk)) posTrans'  -- [tr c' | tr <- posTrans', c' <- [c,Nothing]]
 -- deltaOf = transGraphToDeltaTrans
 
--- delta' :: TransitionFunction -> Maybe Char -> PDAState -> FlatTree PDAState
--- delta' delta c (q,stk) = FTr . pair (q,stk) $ delta c (q,stk)
+-- deltaStar :: TransitionFunction -> String -> PDAState -> Tree PDAState
+-- deltaStar delta [] (q,[]) = Tr ((q,[]),[])
+-- deltaStar delta (x:xs) (q,[]) = Tr ((Q (-1),[]),[])
+-- deltaStar delta [] s      = Tr (s,map (\q -> Tr (q,[])) $ delta Nothing s)
+-- deltaStar delta (c:cs) s  = Tr (s,map (deltaStar delta cs) $ delta (Just c) s ++ delta Nothing s)
 
--- compose :: [a -> a] -> a -> a
--- compose fs v = foldl (flip (.)) id fs $ v
+-- nodeLabel :: Tree PDAState -> PDAState
+-- nodeLabel (Tr (label,_)) = label
+-- nodeChildren :: Tree PDAState -> [Tree PDAState]
+-- nodeChildren (Tr (_,childs)) = childs
+-- computationPaths :: Tree PDAState -> [[PDAState]]
+-- computationPaths (Tr (label,[])) = [[label]]
+-- computationPaths (Tr (label,childs)) = map ((:) label) $ foldl (++) [] $ map computationPaths childs
 
-deltaStar :: TransitionFunction -> String -> PDAState -> Tree PDAState
-deltaStar delta [] (q,[]) = Tr ((q,[]),[])
-deltaStar delta (x:xs) (q,[]) = Tr ((Q (-1),[]),[])
-deltaStar delta [] s      = Tr (s,map (\q -> Tr (q,[])) $ delta Nothing s)
-deltaStar delta (c:cs) s  = Tr (s,map (deltaStar delta cs) $ delta (Just c) s ++ delta Nothing s)
+-- paths :: [a] -> Tree a -> [[a]]
+-- paths hist (Tr (a,[])) = [a:hist]
+-- paths hist (Tr (a,as)) = let hist' = (a:hist) in foldl (++) [] $ map (paths hist') as
 
-nodeLabel :: Tree PDAState -> PDAState
-nodeLabel (Tr (label,_)) = label
-nodeChildren :: Tree PDAState -> [Tree PDAState]
-nodeChildren (Tr (_,childs)) = childs
-computationPaths :: Tree PDAState -> [[PDAState]]
-computationPaths (Tr (label,[])) = [[label]]
-computationPaths (Tr (label,childs)) = map ((:) label) $ foldl (++) [] $ map computationPaths childs
+-- traversals = computationPaths
+-- traversals' = paths []
 
-paths :: [a] -> Tree a -> [[a]]
-paths hist (Tr (a,[])) = [a:hist]
-paths hist (Tr (a,as)) = let hist' = (a:hist) in foldl (++) [] $ map (paths hist') as
+-- acceptingTraversals :: Machine -> Tree PDAState -> [[PDAState]]
+-- acceptingTraversals M{qfs=finals} = filter (flip Set.member finals . fst . head . reverse) . computationPaths
 
-traversals = computationPaths
-traversals' = paths []
-
-acceptingTraversals :: Machine -> Tree PDAState -> [[PDAState]]
-acceptingTraversals M{qfs=finals} = filter (flip Set.member finals . fst . head . reverse) . computationPaths
-
-finalStates :: Tree PDAState -> Set State
-finalStates = Set.fromList . map (fst . head) . traversals'
+-- finalStates :: Tree PDAState -> Set State
+-- finalStates = Set.fromList . map (fst . head) . traversals'
 
 computeMachineOnInput :: Machine -> String -> Tree PDAState
-computeMachineOnInput M{q0=q0,delta=delta} str = deltaStar delta str (q0,"#") 
+computeMachineOnInput = undefined -- M{q0=q0,delta=delta} str = deltaStar delta str (q0,"#") 
 
-findDerivation :: Machine -> String -> [[PDAState]]
-findDerivation m s = filter ((==) (Q(2)) . fst . head) $ traversals' $ computeMachineOnInput m s
+-- findDerivation :: Machine -> String -> [[PDAState]]
+-- findDerivation m s = filter ((==) (Q(2)) . fst . head) $ traversals' $ computeMachineOnInput m s
 
-runPDA :: Machine -> String -> [(String,Bool,[PDAState])]
-runPDA m s = map (\output -> (s,not . null $ output,output)) $ findDerivation m s
+-- runPDA :: Machine -> String -> [(String,Bool,[PDAState])]
+-- runPDA m s = map (\output -> (s,not . null $ output,output)) $ findDerivation m s
+
+runPDA :: Machine -> String -> [(Bool,String,Set PDAState)]
+runPDA m s = map run (inits s)
+  where ds = deltaStar' $ delta m
+        run s' = let fs = ds s' initialState
+                    in ((intersFinSts $ extractStates fs),s',fs)
+        intersFinSts sts = Set.isSubsetOf (qfs m) sts
+        extractStates = Set.map fst
+        initialState = Set.singleton ((q0 m),"#")
 
 composableDelta :: TransitionFunction -> Maybe Char -> Set PDAState -> Set PDAState
 composableDelta d c s = squishSetSet $ Set.map (Set.fromList . d c) s
 
 delta' :: TransitionFunction -> Maybe Char -> Set PDAState -> Set PDAState
--- delta' d c s = let (del,del') = (composableDelta d,runEpsilonTransitions d) 
---                 in let states' = del c $ Set.union (del' s) s
---                     -- in Set.union (del Nothing states') states' 
---                     in states' 
 delta' d c s = let (del,del') = (composableDelta d,runEpsilonTransitions d)
                 in case c of
                     Nothing  -> del' s
@@ -174,28 +130,18 @@ delta' d c s = let (del,del') = (composableDelta d,runEpsilonTransitions d)
                     
           
 deltaStar' :: TransitionFunction -> String -> Set PDAState -> Set PDAState
--- deltaStar' d ""  s = runEpsilonTransitions d s
--- deltaStar' d str s = foldl (flip ($)) s $ map (del . Just) str
---   where del = delta' d
---         -- go (c:cs) 
-deltaStar' d str s = go s str -- foldl (flip ($)) s $ map (del . Just) str
+
+deltaStar' d str s = go s str
   where del = composableDelta d
         del' = runEpsilonTransitions d
-        go s' []     = del' s' -- del Nothing s' -- s'
+        go s' []     = del' s'
         go s' (c:cs) = go (delta' d (Just c) s') cs
-        -- go s' (c:cs) = go (del (Just c) (del' s')) cs
 
--- deltaStar'' d (a:x) s = let del = deltaStar' (d a)
 
 
 
 runEpsilonTransitions :: TransitionFunction -> Set PDAState -> Set PDAState
 runEpsilonTransitions d s = fst $ runEpsilonTransitions' d s
-                            -- let (h,hs) = runEpsilonTransitions' d s
-                            --   in (squishSetList (h:hs)) <&> s
-                              -- in Set.union (foldl Set.union Set.empty hs) s
-
--- runEpsilonTransitions d s = fst $ runEpsilonTransitions' d s
 
 runEpsilonTransitions' :: TransitionFunction -> Set PDAState -> (Set PDAState,[Set PDAState])
 runEpsilonTransitions' d s = go (s,[]) -- go (del s,[])
@@ -204,10 +150,3 @@ runEpsilonTransitions' d s = go (s,[]) -- go (del s,[])
                           in if new == prev
                              then (new,prev : prevs)
                              else go (new,prev : prevs)
-          -- let newS = del prev 
-          --                   in if newS == Set.empty
-          --                      then (prev,reverse prevs)
-          --                      else if newS == prev 
-          --                                 then (newS,reverse $ prev : prevs)
-          --                                 else go (newS,prev : prevs)
-
